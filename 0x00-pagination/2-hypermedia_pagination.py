ca294e2d7f73dc03c19fcbb439733e877b87dea8
Flesier <flesieronyango18@gmail.com>
2023-07-24 10:53:07 +0000

    get_hyper method that takes the same arguments (and defaults) as get_page and returns a dictionary containing the following key-value pairs

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-""" Learning Pagination """
+""" Hypermedia Pagination """
 import csv
-import math
+from math import ceil
 from typing import List, Tuple
 
 
@@ -46,3 +46,31 @@ class Server:
         idx_range = index_range(page, page_size)
         data = self.__dataset[idx_range[0]:idx_range[1]]
         return data
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """ Provides information about dataset """
+        data = self.get_page(page, page_size)
+
+        data_set = self.__dataset
+        len_set = len(data_set) if data_set else 0
+
+        total_pages = ceil(len_set / page_size) if data_set else 0
+
+        if not data:
+            page_size = 0
+        else:
+            page_size = len(data)
+
+        prev_page = page - 1 if page > 1 else None
+        next_page = page + 1 if page < total_pages else None
+
+        hyper = {
+            'page_size': page_size,
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
+
+        return hyper
