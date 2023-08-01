b183bd86f0fd30417217bfa96618e347653f9dca
Flesier <flesieronyango18@gmail.com>
2023-08-01 13:33:51 +0300

    commented docstring

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python3
+"""Basic Flask app in 0-app.py
+"""
+
 from flask import Flask, render_template
 from flask_babel import Babel
+from typing import Any
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -14,9 +19,12 @@ def get_locale():
     return 'en'
 
 #create route
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/', strict_slashes=False)
+def hello_world() -> Any:
+    """Return index.html and output a 
+    message
+    """
+    return render_template('0-index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
