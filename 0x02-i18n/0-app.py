9a18cc9ba81db48036be4f3117dee6a0e60b3002
Flesier <flesieronyango18@gmail.com>
2023-08-01 13:41:31 +0300

    render template to output message

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,30 +1,22 @@
 #!/usr/bin/env python3
-"""Basic Flask app in 0-app.py
 """
-
+setup a basic Flask app in 0-app.py
+"""
 from flask import Flask, render_template
-from flask_babel import Babel
 from typing import Any
 
 app = Flask(__name__)
-babel = Babel(app)
 
-app.config['LANGUAGES'] = {
-    'en': 'English',
-}
 
-#set language to english
-@babel.localeselector
-def get_locale():
-    return 'en'
-
-#create route
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_world() -> Any:
-    """Return index.html and output a 
-    message
     """
-    return render_template('0-index.html')
+    return index.html template that simply outputs
+    “Welcome to Holberton” as page title (<title>) and
+    “Hello world” as header (<h1>)
+    """
+    return render_template("0-index.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
